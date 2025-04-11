a9222ad73a5816068148fa146627733f3abd59ce
Ederson Torresini <etorresini@ifsc.edu.br>
2025-04-11 21:03:24 +0000

    Coletar do MQTT e inserir no InfluxDB

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+import influxdb_client as influxdb
+from influxdb_client.client.write_api import SYNCHRONOUS
+from paho.mqtt import client as mqtt
+from models.payload import Status
+import json
+
+load_dotenv()
+influxdb_url = os.getenv("INFLUXDB_URL", default="http://localhost:8086")
+influxdb_token = os.getenv("INFLUXDB_TOKEN", default="my-token")
+influxdb_org = os.getenv("INFLUXDB_ORG", default="em")
+influxdb_bucket = os.getenv("INFLUXDB_BUCKET", default="em")
+mqtt_broker = os.getenv("MQTT_BROKER", default="localhost")
+mqtt_port = int(os.getenv("MQTT_PORT", default=1883))
+mqtt_client_id = os.getenv("MQTT_CLIENT_ID", default="mqtt-data-logger")
+mqtt_topic = os.getenv("MQTT_TOPIC", default="em/#")
+
+
+def on_message(client, userdata, msg):
+    try:
+        payload_data = json.loads(msg.payload)
+        payload = Status(**payload_data)
+        print(f"Received message: {msg.topic} {payload}")
+
+        fields_to_remove = ["id", "timestamp"]
+        fields = {
+            key: value
+            for key, value in payload.model_dump().items()
+            if key not in fields_to_remove and value is not None
+        }
+        print(f"fields: {fields}")
+
+        dictionary = {
+            "measurement": payload.id,
+            "time": payload.timestamp.isoformat(),
+            "fields": fields,
+        }
+
+        influxdb_client.write_api(write_options=SYNCHRONOUS).write(
+            bucket=influxdb_bucket, org=influxdb_org, record=dictionary
+        )
+    except Exception as e:
+        print(f"Error processing message: {e}")
+
+
+if __name__ == "__main__":
+    influxdb_client = influxdb.InfluxDBClient(
+        url=influxdb_url, token=influxdb_token, org=influxdb_org, bucket=influxdb_bucket
+    )
+
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, mqtt_client_id)
+    mqtt_client.connect(mqtt_broker, mqtt_port, 60)
+    mqtt_client.subscribe(mqtt_topic)
+    mqtt_client.on_message = on_message
+    mqtt_client.loop_forever()
---
+++ models/payload.py
@@ -0,0 +1,17 @@
+from prometheus_client import MetricsHandler
+from pydantic import BaseModel
+from datetime import datetime
+
+class Status(BaseModel):
+    id: str
+    timestamp: datetime = None
+    temperature: float = None
+    humidity: float = None
+    pressure: float = None
+    timestamp: datetime = None
+    temperature: float = None
+    humidity: float = None
+    pressure: float = None
+    co: float = None
+    co2: float = None
+    methane: float = None
