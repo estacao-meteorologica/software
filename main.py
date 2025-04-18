b319a751ce9d4fbc44d42bb1e1c0520ccd020260
Ederson Torresini <etorresini@ifsc.edu.br>
2025-04-18 00:25:56 +0000

    Adaptar mensagem para influx data point

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 import influxdb_client as influxdb
 from influxdb_client.client.write_api import SYNCHRONOUS
 from paho.mqtt import client as mqtt
-from models.payload import Status
 from datetime import datetime
-import json
 
 load_dotenv()
 influxdb_url = os.getenv("INFLUXDB_URL", default="http://localhost:8086")
@@ -14,35 +12,20 @@ influxdb_org = os.getenv("INFLUXDB_ORG", default="em")
 influxdb_bucket = os.getenv("INFLUXDB_BUCKET", default="em")
 mqtt_broker = os.getenv("MQTT_BROKER", default="localhost")
 mqtt_port = int(os.getenv("MQTT_PORT", default=1883))
-mqtt_client_id = os.getenv("MQTT_CLIENT_ID", default="mqtt-data-logger")
+mqtt_client_id = os.getenv("MQTT_CLIENT_ID", default="mqtt-data-logger-v0")
 mqtt_topic = os.getenv("MQTT_TOPIC", default="em/#")
 
 
 def on_message(client, userdata, msg):
     try:
-        payload_data = json.loads(msg.payload)
-        payload = Status(**payload_data)
+        payload = msg.payload.decode("utf-8").split(" ")
 
-        fields_to_remove = ["id", "timestamp"]
-        fields = {
-            key: value
-            for key, value in payload.model_dump().items()
-            if key not in fields_to_remove and value is not None
-        }
-
-        if payload.timestamp is None:
-            payload.timestamp = datetime.now()
-
-        record = {
-            "measurement": payload.id,
-            "time": payload.timestamp.isoformat(),
-            "fields": fields,
-        }
+        record = " ".join(payload)
+        if len(payload) < 3:
+            record = " ".join([record, str(int(datetime.now().timestamp()))])
 
         with influxdb_client.write_api(write_options=SYNCHRONOUS) as writer:
-            p = influxdb.Point.from_dict(record)
-            print(p)
-            writer.write(bucket=influxdb_bucket, org=influxdb_org, record=p)
+            writer.write(bucket=influxdb_bucket, org=influxdb_org, record=record)
     except Exception as e:
         print(f"Error processing message: {e}")
 
